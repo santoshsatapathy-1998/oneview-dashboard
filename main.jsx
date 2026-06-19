1732952cb060c5551c2a8a353ba5fd2387d9ff4a
Santosh Kumar Satapathy <santosh.satapathy@jindalsteel.in>
2026-06-19 11:43:01 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './jindal-oneview-dashboard_v7_final.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
